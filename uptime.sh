14556dec1a986504a63b74c43d6d5b9fea4bb0f4
faustouribe <faustouribe@gmail.com>
2023-04-22 17:03:30 -0500

    Updated volumes

--- uptime.sh
+++ uptime.sh
@@ -1,7 +1,8 @@
 docker run -d \
 -p 3001:3001 \
--v /home/<user>/uptime-kuma:/app/data \
+-v /home/<user>/config/uptime-kuma/data:/app/data \
+-v /home/<user>/config/uptime-kuma/app:/app \
 --name uptime-kuma \
---network rvrsprxy \
+--network proxy \
 --restart=always \
 louislam/uptime-kuma:1
